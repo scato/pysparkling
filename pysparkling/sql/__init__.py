0f4bfecf23472f189c512c04468d23616c161b1d
Scato Eggen <scato.eggen@gmail.com>
2017-09-29 22:56:06 +0200

    Initial migration from pyspark

---
+++ pysparkling/sql/__init__.py
@@ -0,0 +1,60 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Important classes of Spark SQL and DataFrames:
+
+    - :class:`pysparkling.sql.SparkSession`
+      Main entry point for :class:`DataFrame` and SQL functionality.
+    - :class:`pysparkling.sql.DataFrame`
+      A distributed collection of data grouped into named columns.
+    - :class:`pysparkling.sql.Column`
+      A column expression in a :class:`DataFrame`.
+    - :class:`pysparkling.sql.Row`
+      A row of data in a :class:`DataFrame`.
+    - :class:`pysparkling.sql.GroupedData`
+      Aggregation methods, returned by :func:`DataFrame.groupBy`.
+    - :class:`pysparkling.sql.DataFrameNaFunctions`
+      Methods for handling missing data (null values).
+    - :class:`pysparkling.sql.DataFrameStatFunctions`
+      Methods for statistics functionality.
+    - :class:`pysparkling.sql.functions`
+      List of built-in functions available for :class:`DataFrame`.
+    - :class:`pysparkling.sql.types`
+      List of data types available.
+    - :class:`pysparkling.sql.Window`
+      For working with window functions.
+"""
+from __future__ import absolute_import
+
+
+from pysparkling.sql.types import Row
+from pysparkling.sql.context import SQLContext, HiveContext, UDFRegistration
+from pysparkling.sql.session import SparkSession
+from pysparkling.sql.column import Column
+from pysparkling.sql.dataframe import DataFrame, DataFrameNaFunctions, DataFrameStatFunctions
+from pysparkling.sql.group import GroupedData
+from pysparkling.sql.readwriter import DataFrameReader, DataFrameWriter
+from pysparkling.sql.window import Window, WindowSpec
+
+
+__all__ = [
+    'SparkSession', 'SQLContext', 'HiveContext', 'UDFRegistration',
+    'DataFrame', 'GroupedData', 'Column', 'Row',
+    'DataFrameNaFunctions', 'DataFrameStatFunctions', 'Window', 'WindowSpec',
+    'DataFrameReader', 'DataFrameWriter'
+]
